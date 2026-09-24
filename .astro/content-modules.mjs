d1c9f4bb8169e03d27c9343568b2f649a82ee5b9
jhoseph-est <j.ludena.v@uni.pe>
2026-09-24 17:50:56 -0500

    Aplicar .gitignore y dejar de rastrear archivos ignorados

--- .astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/docs/puebas/flashcards.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpuebas%2Fflashcards.mdx&astroContentModuleFlag=true")],
-["src/content/docs/puebas/gantt-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fpuebas%2Fgantt-mdx.mdx&astroContentModuleFlag=true")]]);
-		
